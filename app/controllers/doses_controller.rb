b795c0fa1485c53d1808ed05282dbf0d77b118d5
Ilona Kozenkovaite <kozenkovaite@gmail.com>
2020-06-30 19:38:50 +0200

    Controllers & ingredients, doses and cocktails

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,47 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :edit, :update, :destroy]
+  before_action :set_cocktail, only: [:new, :create]
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+     @dose = Dose.new(dose_params)
+     @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+        redirect_to cocktail_path([@dose.cocktail])
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @dose.destroy
+  end
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
 end
