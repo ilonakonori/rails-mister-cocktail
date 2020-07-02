c8669c6cbb97294c45401975a3279710282959c2
Ilona Kozenkovaite <kozenkovaite@gmail.com>
2020-07-02 19:36:36 +0200

    extra errors fixed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,7 @@
 class DosesController < ApplicationController
   before_action :set_dose, only: [:show, :edit, :update, :destroy]
   before_action :set_cocktail, only: [:new, :create]
+  
   def index
   end
 
